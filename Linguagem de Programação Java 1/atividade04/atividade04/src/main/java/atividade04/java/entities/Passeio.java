d9cb42fd318894bef86be442d7dde7252a0ed769
Caio Mantovani Borba <caiovmborba@gmail.com>
2026-04-15 08:41:41 -0300

    Linguagem de Programação Java 1 - Atividade 04

---
+++ Linguagem de Programação Java 1/atividade04/atividade04/src/main/java/atividade04/java/entities/Passeio.java
@@ -0,0 +1,47 @@
+package atividade04.entities;
+
+public final class Passeio extends Veiculo implements Calcular {
+    private int qtdPassageiros;
+
+    public Passeio() {
+        this.qtdPassageiros = 0;
+    }
+
+    public Passeio(String placa, String marca, String modelo, String cor, float velocMax, int qtdRodas, Motor motor,
+            int qtdPassageiros) {
+        super(placa, marca, modelo, cor, velocMax, qtdRodas, new Motor(motor.getPotencia(), motor.getQtdPist()));
+        this.qtdPassageiros = qtdPassageiros;
+    }
+
+    public int getQtdPassageiros() {
+        return qtdPassageiros;
+    }
+
+    public void setQtdPassageiros(int qtdPassageiros) {
+        this.qtdPassageiros = qtdPassageiros;
+    }
+
+    @Override
+    public float calcVel(float velocMax) {
+        return velocMax * 1000;
+    }
+
+    @Override
+    public int calcular() {
+        int qtdCaracteres = 0;
+
+        qtdCaracteres += getPlaca().length();
+        qtdCaracteres += getMarca().length();
+        qtdCaracteres += getModelo().length();
+        qtdCaracteres += getCor().length();
+
+        return qtdCaracteres;
+    }
+
+    @Override
+    public String toString() {
+        return "Marca := " + getMarca() + "\nModelo := " + getModelo() + "\nPlaca : " + getPlaca()
+                + "\nVelocidade Maxima := " + calcVel(getVelocMax()) + "\nQtd Passageiros := " 
+                + qtdPassageiros + motor.toString() + "\nNúmero de Letras := " + calcular();
+    }
+}
