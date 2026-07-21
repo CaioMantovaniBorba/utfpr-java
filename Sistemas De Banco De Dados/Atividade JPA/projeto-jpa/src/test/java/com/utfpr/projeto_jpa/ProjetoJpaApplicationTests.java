27419824b89211e0a10dfba195325dfe39e1a860
Caio Mantovani Borba <caiovmborba@gmail.com>
2026-07-21 16:17:15 -0300

    Sistemas De Banco De Dados - Atividade JPA - Criar projeto Spring

---
+++ Sistemas De Banco De Dados/Atividade JPA/projeto-jpa/src/test/java/com/utfpr/projeto_jpa/ProjetoJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.utfpr.projeto_jpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
